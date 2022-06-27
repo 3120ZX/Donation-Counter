d2e2dc38c9932561a97a4b6dd799721323c9236b
3120ZX <3120eduardo@gmail.com>
2022-06-27 14:45:15 +0700

    27/06/2022

---
+++ routes/routes_admin.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const Admin = require('./../models/admin.js')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const initializePassport = require('./../passport-config')
+
+
+router.use(flash())
+router.use(session({
+    secret : process.env.SESSION_SECRET,
+    resave : false,
+    saveUninitialized : false
+}))
+initializePassport(
+    passport,
+    username => Admin.findOne(user => user.username === username),
+  	id => Admin.findOne(user => user.id === id)
+)
+
+router.use(passport.initialize())
+router.use(passport.session())
+router.use(methodOverride('_method'))
+
+router.get('/',checkAunthenticated,(req, res) => {
+	console.log('Viewing admin panel')
+	res.render('../views/admin/admin.html')
+})
+
+router.get('/login',checkNotAunthenticated,(req, res) => {
+	console.log('Viewing Login Page')
+	res.render('../views/admin/login.html')
+})
+
+router.post('/login',checkNotAunthenticated, passport.authenticate('local',{
+	successRedirect : '/admin',
+	failureRedirect : '/admin/login',
+	failureFlash : true
+}))
+
+router.get('/register',checkAunthenticated, (req, res) => {
+	console.log('Viewing admin register panel')
+	res.render('../views/admin/register.html')
+})
+
+router.post('/register',checkAunthenticated, async(req,res,next) => {
+	try{
+	const cryptedPassword = await bcrypt.hash(req.body.password, 10)
+	let admin = new Admin({
+			id : Date.now().toString(),
+			username : req.body.username,
+			// password : req.body.password
+			password : cryptedPassword
+		})
+		admin = admin.save()
+		console.log('Register Post')
+		next()
+		res.redirect('/admin')
+	}
+	catch (e){
+		console.log(e)
+		res.redirect('/admin/success')
+	}
+})
+
+router.get('/change_password',checkAunthenticated, (req, res) => {
+	console.log('Viewing admin change password panel')
+	res.render('../views/admin/change_password.html')
+})
+
+router.patch('/change_password',checkAunthenticated, async(req,res,next) => {
+	let search
+	try{
+		search = Admin.findOne({ username: req.body.username }, async(e, user) =>{
+            if (e) return (e);
+            if (!user) {
+				console.log('No Admin With that Username')
+				next();
+				res.redirect('/admin/change_password')
+				return { message: 'Incorrect username / password.' }
+			}
+			else{
+				try{
+					const UpdatedPassword = await bcrypt.hash(req.body.password, 10)
+					await Admin.updateOne(
+						{username : req.body.username},
+						{password : UpdatedPassword}
+					)
+					console.log('password changed')
+					next()
+					res.redirect('/admin')
+				}catch (e){
+					console.log('change password failed')
+					console.log(e)
+					res.redirect('/admin/change_password')
+				}
+			}
+            });
+    }catch (e){
+		console.log(e)
+		res.redirect('/admin/change_password')
+		}
+})
+
+router.delete('/logout', (req, res) => {
+	console.log('logout')
+	req.logOut()
+	res.redirect('/')
+  })
+
+function checkAunthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+	  return next()
+	}
+	res.redirect('/admin/login')
+
+	// if (req.isAuthenticated()) {
+	// 	return res.redirect('/admin')
+	//   }
+	//   next()
+  }
+  
+function checkNotAunthenticated(req, res, next) {
+	
+	if (req.isAuthenticated()) {
+	  return res.redirect('/admin')
+	}
+	next()
+  }
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,47 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const expressLayouts = require ('express-ejs-layouts')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const path = require('path')
+const port = 8000;
+
+var resources = path.join(__dirname+ '/resources')
+var public = path.join(__dirname+ '/public')
+app.use(express.static(resources))
+app.use(express.static(public))
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(expressLayouts)
+app.set('layout','layouts/layout.html')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+console.log('Server start/restart')
+console.log('listening to port',port)
+console.log()
+
+app.get('/',(req,res) => {
+    res.render('index.html')
+    console.log('Viewing Dashboard')
+})
+
+const routes_admin = require('./routes/routes_admin');
+app.use('/admin',routes_admin)
+
+app.listen(port)
